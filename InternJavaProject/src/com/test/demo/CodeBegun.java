ffde3f6aa0c92b7a415a49feda26f1763ef70bab
SuryaKiran namala <suryakiran.namala@gmail.com>
2024-11-15 20:41:35 +0530

    test
    test commit

---
+++ InternJavaProject/src/com/test/demo/CodeBegun.java
@@ -0,0 +1,12 @@
+package com.test.demo;
+
+public class CodeBegun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello CODEBEGUN");
+
+	}
+
+}
